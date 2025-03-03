3e1458786aca8d4f45eaffa03f44cb945733493c
Bircheses <kbrzoza03@outlook.com>
2025-03-03 18:07:58 +0100

    Add state enum for storing different states of philosophers and two macros for calculating left and right fork index number

--- main.cpp
+++ main.cpp
@@ -7,14 +7,26 @@
 
 using namespace std;
 
-void eating(int i){
+enum state {
+    eating,
+    hungry,
+    thinking
+};
+
+int N = 5;
+int philosophers[N];
+
+#define left (= (phindex + 1) % N)
+#define right (= (phindex + N - 1) % N)
+
+void eat(int i){
     cout << "Phliosopher " << i << "started eating" << endl;
     // every philosopher will eat for 100 milliseconds
     this_thread::sleep_for(chrono::milliseconds(100));
     cout << "Philosopher " << i << "stopped eating" << endl;
 }
 
-void thinking(int i){
+void think(int i){
     int time = rand()%1000 + 300;
     cout << "Philosopher " << i << "thinking..." << endl;
     this_thread::sleep_for(chrono::milliseconds(time));
@@ -22,4 +34,7 @@ void thinking(int i){
 
 int main(){
     srand(time(NULL));
+    for(int i=0; i<N; i++) {
+        philosophers[i] = i;
+    }
 }
