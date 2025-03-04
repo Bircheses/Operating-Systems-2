0dd5fb78383ed9b17ecaed942cf2aae7319b76bc
Bircheses <kbrzoza03@outlook.com>
2025-03-04 18:38:01 +0100

    FInish. Added the rest of the mutex logic. Also added semaphore for refreshing the output of the console when the state of the philosopher changes, for which I needed string macros which I created because constant strings or function for both would be more annoying as I needed philNum argument.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,17 @@
 #include <semaphore>
 #include <chrono>
 #include <ctime>
+#include <cstdlib>
 
 using namespace std;
+
+// Left and right philosopher
+#define left ((philNum + 1) % N)
+#define right ((philNum + N - 1) % N)
+
+// Three states in string
+#define thinkString(philNum) cout << "Philosopher " << philNum << " is thinking..." << std::endl;
+#define eatString(philNum) cout << "Philosopher " << philNum << " is eating..." << std::endl;
 
 enum state {
     eating,
@@ -14,46 +23,86 @@ enum state {
 };
 
 int N = 5;
-state philState[N];
-mutex m;
+state *philState;
+mutex criticalMutex;
+binary_semaphore stateChanged{0};
 
-#define left ((philNum + 1) % N)
-#define right ((philNum + N - 1) % N)
-
-void eat(int philNum){
-    cout << "Phliosopher " << philNum << "started eating" << endl;
-    // every philosopher will eat for 2 seconds
-    this_thread::sleep_for(chrono::seconds(2));
-    cout << "Philosopher " << philNum << "stopped eating" << endl;
-}
-
-void think(int philNum){
-    int time = rand()%5 + 2;
-    cout << "Philosopher " << philNum << "thinking..." << endl;
-    this_thread::sleep_for(chrono::seconds(time));
-}
+string output;
 
 void checkAvaibility(int philNum) {
-    if (philState[philNum] == hungry && philState[left] != eating && philState[right] != eating){
+    if (philState[philNum] == hungry && philState[left] != eating && philState[right] != eating) {
+        int randTime = rand() % 2 + 1;
+        criticalMutex.unlock();
         philState[philNum] = eating;
-        eat(philNum);
+        // Starts eating from 1 to 2 seconds
+        stateChanged.release(); // Send the signal to write function to refresh the output
+        this_thread::sleep_for(chrono::seconds(randTime));
+    } else {
+        criticalMutex.unlock();
     }
 }
 
 //Using dikstra's convention, which means that one philosopher will try to pick up two forks at the same time
 void pickUpForks(int philNum) {
-    m.lock();
+    criticalMutex.lock();
     philState[philNum] = hungry;
+    checkAvaibility(philNum);
 }
 
-void releaseForks(int phnum) {
-
+void releaseForks(int philNum) {
+    int randTime = rand() % 5 + 2;
+    philState[philNum] = thinking;
+    // Starts thinking from 2 to 5 seconds
+    stateChanged.release(); // Send the signal to write function to refresh the output
+    this_thread::sleep_for(chrono::seconds(randTime));
 }
 
-int main(){
+void start(int thNum) {
+    while (true) {
+        pickUpForks(thNum);
+        releaseForks(thNum);
+    }
+}
+
+void write() {
+    while(true) {
+        stateChanged.acquire(); // Waits for signal from thread function to refresh output
+        system("cls");
+        for (int philNum = 0; philNum < N; philNum++) {
+            switch (philState[philNum]) {
+                case thinking:
+                    thinkString(philNum);
+                break;
+                case eating:
+                    eatString(philNum);
+                break;
+                default:
+                    thinkString(philNum);
+                break;
+            }
+        }
+    }
+}
+
+int main() {
     srand(time(NULL));
 
-    for(int i=0; i<N; i++) {
+    auto *threads = new thread[N];
+    philState = new state[N];
+
+    for (int i = 0; i < N; i++) {
         philState[i] = thinking;
+        threads[i] = thread(start, i);
     }
+
+    auto writeThread = new thread(write);
+
+    for (int i = 0; i < N; i++) {
+        threads[i].join();
+    }
+
+    delete [] threads;
+    delete [] philState;
+
+    return 0;
 }
